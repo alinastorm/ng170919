74c26e9becb437e9981ba23c593a17d59d9f9db9
Alex Guriev <sasaguryev@mail.ru>
2019-09-25 21:27:42 +0300

    toolbar material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import {MatToolbarModule} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule
+  BrowserModule,
+  BrowserAnimationsModule,
+  MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
